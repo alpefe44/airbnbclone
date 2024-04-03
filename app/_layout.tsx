ce9157ff15446d9a36888b44dab797a75d6c6018
alpefe44 <efe1998alp@hotmail.com>
2024-04-03 18:16:02 +0300

    bottomsheet

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Stack, Tabs } from 'expo-router'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 const RootLayout = () => {
   return (
-    <Stack/>
-
+    <GestureHandlerRootView style = {{flex : 1}}>
+      <Stack />
+    </GestureHandlerRootView>
   )
 }
 
--- app/airbnb.tsx
+++ app/airbnb.tsx
@@ -1,15 +1,18 @@
 import { View, Text, StyleSheet, Pressable } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import MapView, { Marker } from 'react-native-maps'
 import { Stack } from 'expo-router'
 
 import locations from '../assets/data/apartments.json'
 import LocationItem from '../src/component/LocationItem'
+import { LocationType } from '../src/types'
+
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet'
 
 const AirbnbScreen = () => {
 
-    const [selectedLocation, setSelectedLocation] = useState(null)
-
+    const [selectedLocation, setSelectedLocation] = useState<LocationType | null>(null)
+    const bottomSheetRef = useRef<BottomSheet>(null);
     const [mapRegion, setMapRegion] = useState({
         latitude: 37.78825,
         longitude: -122.4324,
@@ -17,6 +20,8 @@ const AirbnbScreen = () => {
         longitudeDelta: 0.0421,
     });
 
+
+    const snapPoints = useMemo(() => [50, '25%', '50%'], [])
 
 
     return (
@@ -32,7 +37,12 @@ const AirbnbScreen = () => {
                                 latitude: item.latitude,
                                 longitude: item.longitude
                             }}
-                        ></Marker>
+                        >
+                            <View style={{ backgroundColor: 'white', padding: 3, borderWidth: 1, borderColor: 'gray', borderRadius: 16 }}>
+                                <Text>{item.price}$</Text>
+                            </View>
+
+                        </Marker>
                     )
                 }
             </MapView>
@@ -40,6 +50,16 @@ const AirbnbScreen = () => {
             {
                 selectedLocation && <LocationItem item={selectedLocation}></LocationItem>
             }
+
+            <BottomSheet
+                ref={bottomSheetRef}
+                snapPoints={snapPoints}
+            >
+                <BottomSheetView style={styles.contentContainer}>
+                    <Text>Awesome 🎉</Text>
+                </BottomSheetView>
+            </BottomSheet>
+
         </View>
     )
 }
@@ -50,6 +70,10 @@ const styles = StyleSheet.create({
     map: {
         width: '100%',
         height: '100%',
+    },
+    contentContainer: {
+        flex: 1,
+        alignItems: 'center',
     },
 })
 
